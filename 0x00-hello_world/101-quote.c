7fc5e127c9e5f88ea22a46fb70f1bf3fa15a43e8
DenzelRSA <t.d.selokela@gmail.com>
2023-03-16 14:25:31 -0700

    pufated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 
 /**
  *main - print "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a newline, to standard error. 
- * Return Always 1.
+ *followed by a newline, to standard error. 
+ *Return Always 1.
  */
 
 int main(void)
