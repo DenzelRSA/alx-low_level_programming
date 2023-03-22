d596722d49f3a6a3219ededefd9b919a61d76019
DenzelRSA <t.d.selokela@gmail.com>
2023-03-22 07:01:12 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two values
+ * @n1: The first value to be computed
+ * @n2: The second value to be computed
+ * REturn: integer value
+ */
+
+int add(int n1, int n2)
+
+{
+	return(n1 + n2);
+}
