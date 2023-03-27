dc501148b5a61de8c76db35b24af9d75fd029368
DenzelRSA <t.d.selokela@gmail.com>
2023-03-27 09:02:30 -0700

    rest to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * reset_to_98 - take apointer to an int and updates
+ * the value to 98
+ *
+ * @n: an inte input pointer
+ * Return: nothing
+ */
+
+void rest_to_98(int *n)
+{
+
+	*n = 98;
+
+}
