05cecf220af9caa3a9bc173fb821484a6f98c637
DenzelRSA <t.d.selokela@gmail.com>
2023-03-28 12:13:16 -0700

    upddated

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,12 +13,12 @@ int _atoi(char *s)
 	unsigned int ni =0;
 	int min = 1;
 	int isi = 0;
-	
+
 	while (s[c])
 	{
 	if (s[c] == 45)
 	{
-	min *= -1;
+	min *= - 1;
 	}
 	while (s[c] >= 48 && s[c] <= 57)
 	{
