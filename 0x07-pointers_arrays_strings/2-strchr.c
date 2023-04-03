14741ce92b37e0711cd76259b8e2e9428a668f39
DenzelRSA <t.d.selokela@gmail.com>
2023-04-03 09:23:37 -0700

    strspn

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,16 +5,18 @@
  * _strchr - Entry point
  * @s: input
  * @c: input
- * REturn: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
+
 char *_strchr(char *s, char c)
+
 {
 	int i;
 
 	for (i = 0; s[i] >= '\0'; i++)
 	{
-		if (s[i] == c)
-			return (s + i);
+	if (s[i] == c)
+	return (s + i);
 	}
 	return (NULL);
 }
